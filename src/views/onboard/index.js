d2e80c37303e7c8ff66f6d4256ace2e3438295d4
Ryan Baker <ryan.da.baker@gmail.com>
2019-12-02 21:47:44 -0500

    Updates onboarding copy

--- src/views/onboard/index.js
+++ src/views/onboard/index.js
@@ -68,16 +68,15 @@ class Onboarding extends Component {
       <div className="metrecord-onboarding-container">
         <h1>Get started</h1>
         <p>follow these simple steps to get started with Metrecord</p>
-        <h3>1. Install the embed</h3>
+        <h3>1. Copy the snippet</h3>
         <p>Click the snippet to copy it to your clipboard</p>
         <pre className="metrecord-snippet" onClick={this.copySnippet}>{getSnippet(clientId)}</pre>
         <textarea readOnly={true} className="metrecord-snippet-textarea" ref={elt => this.textArea = elt} value={getSnippet(clientId)} />
         <div className={cx('metrecord-snippet-copied--text', { visible: copied })}>copied to clipboard!</div>
-        <h3>2. Capture an event</h3>
-        <p>Try recording how many page views you get!</p>
-        <pre>metrecord.track('page_view')</pre>
+        <h3>2. Paste into your website code</h3>
+        <p>Metrecord will automatically gather page load information!</p>
         <h3>3. View metrics</h3>
-        <p>Take a look at <Link to="/home">the dashboard view</Link> to see your default dashboard</p>
+        <p>Take a look at <Link to="/home">the home view</Link> to see your browser load metrics</p>
       </div>
     );
   }
